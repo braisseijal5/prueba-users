a3c52dd51634a1d03ecc82b83fdfc18ca58f1341
braisseijal5 <braisseijal@gmail.com>
2025-03-31 20:36:35 +0200

    texto en español, LISTO

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -38,7 +38,7 @@ export const Pagination = ({
         disabled={actualPage === 1}
         className="flex gap-2 items-center justify-center rounded-lg px-2 py-2 text-sm font-semibold transition-colors duration-200 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-1 disabled:opacity-50 disabled:cursor-not-allowed bg-slate-800 text-white hover:bg-slate-700 border border-slate-700  focus:ring-blue-500 disabled:bg-gray-200 shadow-sm"
       >
-        <span>Previous</span>
+        <span>Anterior</span>
       </button>
       {generarNumeros()}
       <button
@@ -46,7 +46,7 @@ export const Pagination = ({
         disabled={actualPage === totalPages}
         className="flex gap-2 items-center justify-center rounded-lg px-2 py-2 text-sm font-semibold transition-colors duration-200 cursor-pointer focus:outline-none focus:ring-2 focus:ring-offset-1 disabled:opacity-50 disabled:cursor-not-allowed bg-slate-800 text-white hover:bg-slate-700 border border-slate-700  focus:ring-blue-500 disabled:bg-gray-200 shadow-sm"
       >
-        <span>Next</span>
+        <span>Siguiente</span>
       </button>
     </div>
   );
